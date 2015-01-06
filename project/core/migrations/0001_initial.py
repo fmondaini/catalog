25efe8dd646d3bdc4f1ced558f1048270c635ec7
Filipe Mondaini <filipe@mondaini.me>
2015-01-05 22:34:13 -0200

    Adicionando model base + models da aplicacao

---
+++ project/core/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Aplicacao',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='data de cria\xe7\xe3o')),
+                ('modified', models.DateTimeField(auto_now=True, verbose_name='data de modifica\xe7\xe3o', auto_now_add=True)),
+                ('name', models.CharField(max_length=25)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Servidor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='data de cria\xe7\xe3o')),
+                ('modified', models.DateTimeField(auto_now=True, verbose_name='data de modifica\xe7\xe3o', auto_now_add=True)),
+                ('name', models.CharField(max_length=25, verbose_name=b'hostname')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- project/core/models.py
+++ project/core/models.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, verbose_name=u'data de criação')
+    modified = models.DateTimeField(auto_now_add=True, auto_now=True, verbose_name=u'data de modificação')
+
+    class Meta:
+        abstract = True
+
+
+class Servidor(TimeStampedModel):
+    name = models.CharField(max_length=25, verbose_name='hostname')
+    # aplicacoes
+
+    def __unicode__(self):
+        return self.name
+
+
+class Aplicacao(TimeStampedModel):
+    name = models.CharField(max_length=25)
+    # status
+
+    def __unicode__(self):
+        return self.name
