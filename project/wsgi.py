ae97226482821fb0024163072ced5596db96a303
Filipe Mondaini <filipe@mondaini.me>
2015-01-10 19:40:40 -0200

    Mudando maneira de servir os estáticos

--- project/wsgi.py
+++ project/wsgi.py
@@ -5,5 +5,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
