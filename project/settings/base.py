32e4427bc40644659d2a0552e5d02ecf2e2dd5fc
Filipe Mondaini <filipemondaini@gmail.com>
2015-01-12 11:42:00 -0200

    Atualizando settings base

--- project/settings/base.py
+++ project/settings/base.py
@@ -82,6 +82,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 TASTYPIE_DEFAULT_FORMATS = ['json']
